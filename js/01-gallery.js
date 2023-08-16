60ae1bdae8fee658ca786b433bdcaf7383013f33
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-08-16 12:25:59 +0400

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,64 +1,50 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
 
+import { galleryItems } from "./gallery-items.js";
 
 function createGalleryMarkup(galleryObj) {
-    return galleryObj
-        .map(
-        ({ preview, original, description }) => 
-`
-<li class = "gallery__item">
-<a class = "gallery__link" href="${original}">
-<img
-    class = "gallery__image"
-    src="${preview}"
-    data-source = "${original}"
-    alt="${description}"
-/>
-</a>
-</li>
-`
+  return galleryObj
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
     )
-    .join('');
+    .join("");
 }
 
-
-const galleryRef = document.querySelector('.gallery'); 
+const galleryRef = document.querySelector(".gallery");
 galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
-galleryRef.addEventListener("click", onClick)
-
+galleryRef.addEventListener("click", onClick);
 let modalImage;
 
-function onClick(event) {
-    event.preventDefault();
-    if (!event.target.classList.contains("gallery__item")) {
-        return;
-    }
- 
-    const currentListItem = event.target.getAttribute("data-source");
-    openModal(currentListItem);
-
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const urlOriginalImage = evt.target.getAttribute("data-source");
+  openModal(urlOriginalImage);
 }
 
-function openModal(currentListItem) {
-    modalImage = basicLightbox.create (
-        `<img src="${currentListItem} width="800" height = "600">`
-    );
-    modalImage.show();
-    window.addEventListener("keydown", clickOnEsc); 
+function openModal(urlOriginalImage) {
+  modalImage = basicLightbox.create(`
+<img src="${urlOriginalImage}" width="800" height="600">
+`);
+  modalImage.show();
+  window.addEventListener("keydown", clickOnEsc);
 }
 
-function clickOnEsc(event) {
-    if (event.code === "Escape" && modalImage.visible()) {
-        modalImage.close();
-        window.removeEventListener("keydown", clickOnEsc);
-       }
-   }
-    
-
-
-
-
-
+function clickOnEsc(evt) {
+  if (evt.code === "Escape" && modalImage.visible()) {
+    modalImage.close();
+    window.removeEventListener("keydown", clickOnEsc);
+  }
+}
