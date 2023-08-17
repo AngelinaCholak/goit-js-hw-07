bbe5ff03e43cca5721ebbc6e1994ad62c8a99d7e
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-08-17 18:00:43 +0400

    hw-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+function createGalleryMarkup(galleryObj) {
+  return galleryObj
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+    )
+    .join("");
+}
+
+const galleryRef = document.querySelector(".gallery");
+galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+galleryRef.addEventListener("click", onClick);
+let modalImage;
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const urlOriginalImage = evt.target.getAttribute("data-source");
+  openModal(urlOriginalImage);
+}
+
+function openModal(urlOriginalImage) {
+  modalImage = basicLightbox.create(`
+// <img src="${urlOriginalImage}" width="800" height="600">
+// `);
+  modalImage.show();
+  window.addEventListener("keydown", clickOnEsc);
+}
+
+function clickOnEsc(evt) {
+  if (evt.code === "Escape" && modalImage.visible()) {
+    modalImage.close();
+    window.removeEventListener("keydown", clickOnEsc);
+  }
+}
